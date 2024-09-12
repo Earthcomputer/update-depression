1091297bcff19db312237738c14b2091e21e3d75
VelizarBG <31567122+VelizarBG@users.noreply.github.com>
2024-09-12 16:15:54 +0300

    Add noise suppression command and update Gradle and Loom (#3)
    Co-authored-by: joe <burtonjae@hotmail.co.uk>

--- src/main/java/net/earthcomputer/updatedepression/command/UpdateDepressionCommands.java
+++ src/main/java/net/earthcomputer/updatedepression/command/UpdateDepressionCommands.java
@@ -7,5 +7,6 @@ import net.minecraft.commands.CommandSourceStack;
 public class UpdateDepressionCommands {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher, CommandBuildContext context) {
         SetSuppressedShulkerBoxCommand.register(dispatcher, context);
+        SetNoiseSuppressorCommand.register(dispatcher, context);
     }
 }
